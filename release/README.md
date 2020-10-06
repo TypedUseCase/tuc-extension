72aeeca463d27fa32a9ec84c4b53d6e707419d95
Petr Chromec <mortal_flesh@seznam.cz>
2020-10-06 11:57:11 +0200

    Use F# for extension source (inspired by Ionide)

---
+++ release/README.md
@@ -0,0 +1,20 @@
+TypedUseCase - .tuc
+===================
+
+[![Version](https://vsmarketplacebadge.apphb.com/version/MortalFlesh.tuc.svg)](https://marketplace.visualstudio.com/items?itemName=MortalFlesh.tuc)
+[![tuc-docs](https://img.shields.io/badge/documentation-tuc-orange.svg)](https://mortalflesh.github.io/tuc-console/)
+
+> Extension for visual studio code, which add support for TUC language.
+
+## TUC
+> **T**yped **U**se-**C**ase
+
+![tuc-logo](https://raw.githubusercontent.com/MortalFlesh/tuc-extension/master/release/images/tuc-logo.png)
+
+It is basically a use case definition, for which this console application can generate [PlantUML](https://plantuml.com/) diagram, where all services are domain specific type safe.
+
+For more information, go check [tuc-console](https://github.com/MortalFlesh/tuc-console) or [documentation](https://mortalflesh.github.io/tuc-console/).
+
+## Syntax Highlighting
+
+For now, the only thing it does is a syntax highlighting for keywords, some participants and most common errors.
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+var path = require("path");
+var webpack = require("webpack");
+var fableUtils = require("fable-utils");
+var nodeExternals = require('webpack-node-externals');
+
+function resolve(filePath) {
+  return path.join(__dirname, filePath)
+}
+
+
+var babelOptions = fableUtils.resolveBabelOptions({
+  presets: [["es2015", { "modules": false }]],
+  plugins: ["transform-runtime"]
+});
+
+var isProduction = process.argv.indexOf("-p") >= 0;
+console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
+
+module.exports = function (env) {
+
+  var outputPath = "out";
+  console.log("Output path: " + outputPath);
+
+  var compilerDefines = isProduction ? [] : ["DEBUG"];
+
+  return {
+    target: 'node',
+    devtool: "source-map",
+    entry: resolve('./src/Extension.fsproj'),
+    output: {
+      filename: 'extension.js',
+      path: resolve('./' + outputPath),
+      //library: 'IONIDEFSHARP',
+      libraryTarget: 'commonjs'
+    },
+    resolve: {
+      modules: [resolve("./node_modules/")]
+    },
+    //externals: [nodeExternals()],
+    externals: {
+      // Who came first the host or the plugin ?
+      "vscode": "commonjs vscode",
+
+      // Optional dependencies of ws
+      "utf-8-validate": "commonjs utf-8-validate",
+      "bufferutil": "commonjs bufferutil"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.fs(x|proj)?$/,
+          use: {
+            loader: "fable-loader",
+            options: {
+              babel: babelOptions,
+              define: compilerDefines
+            }
+          }
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: babelOptions
+          },
+        }
+      ]
+    }
+  };
+}
